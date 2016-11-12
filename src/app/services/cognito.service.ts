b7a9065de4a8e012c28561b2e9891766137a2094
David Sudia <dsudia@gmail.com>
2016-11-12 11:38:23 -0700

    hold this

--- src/app/services/cognito.service.ts
+++ src/app/services/cognito.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, Inject } from '@angular/core'
-import RegistrationUser from '../public/auth/RegistrationUser'
+import { Injectable, Inject } from '@angular/core';
+import RegistrationUser from '../public/auth/RegistrationUser';
 
 declare let AWS: any;
 declare let AWSCognito: any;
@@ -22,8 +22,8 @@ export class CognitoUtil {
     public static _REGION = 'us-east-1';
     
     public static _IDENTITY_POOL_ID = 'us-east-1:1589ee70-aa37-4e78-a753-05ecdd3689a3';
-    public static _USER_POOL_ID = 'us-east-1_hodxY4XR9'
-    public static _CLIENT_ID = '66e6j6i6pobdifulaqmasiuqs3'
+    public static _USER_POOL_ID = 'us-east-1_hodxY4XR9';
+    public static _CLIENT_ID = '66e6j6i6pobdifulaqmasiuqs3';
 
     public static _POOL_DATA = {
         UserPoolId: CognitoUtil._USER_POOL_ID,
@@ -43,8 +43,13 @@ export class CognitoUtil {
     }
 
     public static getAccessToken(callback: Callback): void {
-        if (calback === null) {
-            
+        if (callback === null) {
+            throw('callback in getAccessToken is nul ... returning')
         }
+        CognitoUtil.getCurrentUser().getSession(function(serr, session) {
+            if (err) {
+                console.log(`Can't set the credentials: ${err}`)
+            }
+        })
     }
 }
