e219fa683a22e916bb53850906e50b402b706ad7
David Sudia <dsudia@gmail.com>
2016-12-17 13:37:58 -0700

    changed css to scss, added bourbon, neat, bitters, animate

--- src/app/public/landing.component.ts
+++ src/app/public/landing.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'landing',
   templateUrl: './landing.html',
-  styleUrls: ['./landing.css']
+  styleUrls: ['./landing.scss']
 })
 export class LandingComponent { }
